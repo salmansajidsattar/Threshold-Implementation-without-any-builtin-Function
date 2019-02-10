109ad02fb4071d2f1d9a7fcaaebf3dc90f5b6395
salmansajidsattar <38104294+salmansajidsattar@users.noreply.github.com>
2019-02-10 23:53:27 +0500

    Add files via upload

---
+++ Threshold.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+path='DATA\\v3.mp4'
+cap=cv.VideoCapture(path)
+while True:
+    ret,frame=cap.read()
+    if ret is False:
+        break
+    frame=cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    for i in range(720):
+        for j in range(1280):
+            if ((frame[i, j] >= 15) and (frame[i, j] <= 100)):
+                frame[i, j]=255;
+            else:
+                frame[i,j]=0;
+    cv.imshow('IMAGE',frame)
+    k=cv.waitKey(1)
+    print('1')
+    if k is ord('q'):
+        break
